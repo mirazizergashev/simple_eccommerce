734aa818dc460643e550a477024dd25adfa45997
mirazizergashev <mirazizmirmuxsinovich@gmail.com>
2024-06-22 20:11:06 +0500

    ok

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.set('view engine', 'ejs');
 // Static fayllar uchun papka
 app.use(express.static('public'));
 
+// Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Bosh sahifa
 app.get('/', (req, res) => {
     res.render('index');
@@ -31,6 +35,29 @@ app.get('/product/:id', (req, res) => {
         const products = JSON.parse(data);
         const product = products.find(p => p.id == productId);
         res.render('product', { product: product });
+    });
+});
+
+// Yangi mahsulot qo'shish formasi
+app.get('/add-product', (req, res) => {
+    res.render('add-product');
+});
+
+// Yangi mahsulot qo'shish
+app.post('/add-product', (req, res) => {
+    fs.readFile('./data/products.json', (err, data) => {
+        if (err) throw err;
+        let products = JSON.parse(data);
+        const newProduct = {
+            id: products.length ? products[products.length - 1].id + 1 : 1,
+            name: req.body.name,
+            price: req.body.price
+        };
+        products.push(newProduct);
+        fs.writeFile('./data/products.json', JSON.stringify(products, null, 2), (err) => {
+            if (err) throw err;
+            res.redirect('/products');
+        });
     });
 });
 
